b995fd6da71a1e985f126c887fee733538d1805f
mohanmajee <66526101+mohanmajee@users.noreply.github.com>
2022-09-13 21:59:18 +0530

    Day4 Snake and Ladder Problem

---
+++ Day4/Day4SnakeLadder/Uc1.java
@@ -0,0 +1,12 @@
+package Day4SnakeLadder;
+
+public class Uc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
+
+	}
+
+}
