d2b974d1756e4802a751670c9b0a96878a295a37
mohanmajee <66526101+mohanmajee@users.noreply.github.com>
2022-09-30 23:39:06 +0530

    Day 11-12 Practice Problem

---
+++ day11_12/stockAccount/Uc2/StockManagementSystem.java
@@ -0,0 +1,21 @@
+package Uc2;
+
+import java.util.Scanner;
+
+public class StockManagementSystem {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Welcome To Stock Management System");
+        Scanner sc = new Scanner(System.in);
+        StockFunction stockFunction = new StockFunction();
+        System.out.println("Add Stock");
+        stockFunction.addStock();
+        System.out.println("*********************************");
+        System.out.println("Stock Report are : ");
+        stockFunction.stockReport();
+        System.out.println("*********************************");
+        stockFunction).credit();
+        stockFunction.debit();
+	}
+
+}
