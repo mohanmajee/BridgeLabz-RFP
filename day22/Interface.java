a2d24ee7a9c4507042d949af79f9b24b490f4c6d
mohanmajee <66526101+mohanmajee@users.noreply.github.com>
2022-10-23 07:58:27 +0530

    Day 22 Practice Problem
    Address Book Problem using Collection Library

---
+++ day22/Interface.java
@@ -0,0 +1,22 @@
+package day22;
+
+public interface Interface {
+
+	void createContact();
+    void addContact();
+    void editContact();
+    void deleteContact();
+    void display();
+    void displayBook();
+    void addToBook();
+    void operation();
+    void searchPerson();
+    void addContactByCity();
+    void viewByCity();
+    void addContactByState();
+    void viewByState();
+    void sortAddressBook();
+    void sortByCity();
+    void sortByState();
+
+}
