62538af9abf2609ad6b9d5d3b6ffc8ebc49d0b34
mohanmajee <66526101+mohanmajee@users.noreply.github.com>
2022-11-13 22:29:06 +0530

    Day 30 Practice Problem

---
+++ day30/InvoiceService.java
@@ -0,0 +1,17 @@
+package day30;
+
+public class InvoiceService {
+	 private static  String invoice ;
+	    private String userID;
+	    private Ride[] rides;
+	    private InvoiceSummary totalfare;
+
+	    public InvoiceService(String userID, Ride[] rides) {
+	        this.userID = userID;
+	        this.rides = rides;
+	        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	        this.totalfare = invoiceGenerator.calculateFare(rides);
+	        this.invoice = userID+", "+rides+", "+totalfare;
+	    }
+
+}
